7e6b84e5288f3c126c3a36a43d9bfd6862f7ecf8
Joni <joni.koskinen@cs.tamk.fi>
2019-11-19 16:58:40 +0200

    Delete all files related to the game

--- src/main/java/com/github/jonizei/mygameengine/App.java
+++
@@ -1,9 +0,0 @@
-package com.github.jonizei.mygameengine;
-
-public class App
-{
-    public static void main( String[] args )
-    {
-        GameLauncher launcher = new GameLauncher(new MyGame());
-    }
-}
